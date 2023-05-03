8fc1bb50d5852853c51dcc6ed7802dcea85b4cf1
allansingi <allansingi@gmail.com>
2023-05-03 21:34:09 +0100

    new project

---
+++ src/test/java/com/allanborges/restaurantAPI/RestaurantApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allanborges.restaurantAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
